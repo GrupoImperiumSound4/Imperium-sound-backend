3a366006004ada86e77694fda8f4851ecbb2eb8f
Steven Alvarez <Stevenyhal@gmail.com>
2025-10-27 22:34:52 -0500

    ahora si la hija de puta url

--- database.py
+++ database.py
@@ -7,7 +7,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-URL_DATABASE = os.getenv("URL_DATABASE")
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+URL_DATABASE = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 if not URL_DATABASE:
     raise ValueError(
