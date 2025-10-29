c380d60ce3ffa2238551a9014844855ac3584afa
Steven Alvarez <Stevenyhal@gmail.com>
2025-10-28 19:51:48 -0500

    con ayuda de Dios

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000", 
     "https://imperium-sound-frontend-olby.vercel.app", 
-    "https://imperium-sound-backend-oj5qrsoj5-unkwonlifters-projects.vercel.app/",
+    "https://imperium-sound-backend.vercel.app/",
     "http://localhost:8000"
 ]
 app.add_middleware(
